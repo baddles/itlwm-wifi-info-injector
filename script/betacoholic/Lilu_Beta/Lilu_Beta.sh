f63cc7d6d083fd92d999bce143edf2dfdd9fe365
Baddles Nguyễn <baddles@Baddless-MacBook-Pro.local>
2020-11-11 10:27:46 +0700

    Initial Commit

--- script/betacoholic/Lilu_Beta/Lilu_Beta.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-#git clone https://github.com/acidanthera/Lilu.git
-dir="${0%/*}"
-mkdir "$dir/Product/Debug"
-mkdir "$dir/Product/Release"
-xcodebuild -project ./Lilu/Lilu.xcodeproj -configuration Debug CONFIGURATION_BUILD_DIR=$(echo "$dir"/Product/Debug/)
-
-xcodebuild -project ./Lilu/Lilu.xcodeproj -configuration Release CONFIGURATION_BUILD_DIR=$(echo "$dir"/Product/Release/)
